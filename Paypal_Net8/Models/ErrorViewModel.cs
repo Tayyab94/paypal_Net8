b9de1dfc0dd4d7fe5755cd1c76130fce458ce001
Tayyab94 <tayyab.bhatti30@gmail.com>
2024-09-07 23:00:27 +0500

    first commit

---
+++ Paypal_Net8/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Paypal_Net8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Paypal_Net8/Program.cs
@@ -0,0 +1,37 @@
+using Paypal_Net8.Clients;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton(s =>
+
+    new PaypalClient(
+        builder.Configuration["PayPalOptions:ClientId"].ToString(),
+        builder.Configuration["PayPalOptions:ClientSecret"],
+        builder.Configuration["PayPalOptions:Mode"]
+        )
+);
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
